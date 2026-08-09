32606c4acb7e56b260fc699b9af6745d80626758
Andrei Teodor <92058834+popr2801@users.noreply.github.com>
2026-08-09 09:46:24 -0400

    Update system_info.h

--- system_info.h
+++ system_info.h
@@ -1,9 +1,11 @@
 #ifndef SYSTEM_INFO_H
 #define SYSTEM_INFO_H
+#include <stdio.h>
+
 
 typedef struct{
-	char hostname[256]
-	char kernel[128];
+	char hostname[256];
+	char kernel[256];
 	char os_name[128];
 	char architecture[64];
 	char current_user[64];
@@ -11,13 +13,13 @@ typedef struct{
 	char cpu_model[256];
 	unsigned int cpu_cores;
 	
-	unsigned total_ram
-	unsigned long free_ram
+	unsigned total_ram;
+	unsigned long free_ram;
 	
 	unsigned long uptime;
 }SystemInfo;
 
 int get_system_info(SystemInfo *info);
-void print_system_info(const SystemInfo *info);
+void print_system_info(SystemInfo *info,FILE *output);
 
 #endif
